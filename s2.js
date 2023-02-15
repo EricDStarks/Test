23654bb2b6c71bb1cbf4fc40246db112e257c23d
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-02-15 10:26:26 -0600

    s2

--- s2.js
+++ s2.js
@@ -1,8 +1,4 @@
-function Warning() 
-
-let speed = 90;
-if (speed > 80) {
-  console.log("Warning: You are driving above 80 mph");
-} else {
-  console.log("You are driving within the speed limit");
-}
+function greet(name) {
+    return "Hello, " + name;
+  }
+  greet("Grace Hopper");
